186f6582748df2bd78b54ba3ed5d2854026260ea
Max U <maxuberas@gmail.com>
2024-05-10 08:49:23 +0800

    remove the path

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 import express from "express";
 import axios from "axios";
-import path from "path";
 
 const app = express();
 const port = 3000;
-
-// Set the views directory
-app.set('views', path.join(__dirname, 'views'));
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
 
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get("https://sfl.world/api/v1/prices");
         const finalRes = result.data.data.p2p;
         console.log(finalRes)
-        res.render("index.ejs", {finalRes: finalRes} )
+        res.render("index.ejs", {finalRes: finalRes["Sunflower"]} )
     } catch (error) {
         const message = "Failed to make request";
         console.error(message);

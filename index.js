ca16eafc9ff6abee671235a7043e26d39b2487de
Max U <maxuberas@gmail.com>
2024-05-10 08:00:09 +0800

    added data and axios

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express"
+import axios from "axios"
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send('Express Vercel app response.')
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get("https://sfl.world/api/v1/prices");
+        const finalRes = result.data.data.p2p;
+        console.log(finalRes)
+        res.send(finalRes)
+    } catch (error) {
+        const message = "Failed to make request";
+        console.error(message);
+        res.send(message);
+    }
 })
 
 app.listen(port, () => {

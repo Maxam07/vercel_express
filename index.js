fedb1cffb2b390854233f112eb77498bea7bf7e6
Max U <maxuberas@gmail.com>
2024-05-10 08:21:55 +0800

    added path and ejs as view engine

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import express from "express"
-import axios from "axios"
+import express from "express";
+import axios from "axios";
+import path from "path";
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 const port = 3000;
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get("https://sfl.world/api/v1/prices");
         const finalRes = result.data.data.p2p;
         console.log(finalRes)
-        res.send(finalRes)
+        res.render("index.ejs", {finalRes: finalRes})
     } catch (error) {
         const message = "Failed to make request";
         console.error(message);

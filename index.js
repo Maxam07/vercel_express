b0c2fe432840e35249d30594e5cedda070907c11
Max U <maxuberas@gmail.com>
2024-05-10 08:35:07 +0800

    minor path changes

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ import express from "express";
 import axios from "axios";
 import path from "path";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 const port = 3000;
 
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
 // Set EJS as the view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get("https://sfl.world/api/v1/prices");
         const finalRes = result.data.data.p2p;
         console.log(finalRes)
-        res.render("index.ejs", {finalRes: finalRes})
+        res.render("index.ejs", {finalRes: finalRes} )
     } catch (error) {
         const message = "Failed to make request";
         console.error(message);
